ffc8a7a10d8b678d421aca981925fe52d3490427
Ghazi Naceur <ghazi.naceur@persgroep.net>
2022-11-09 14:00:26 +0100

    Update company logos and photos urls

--- job-matcher/src/main/scala/dpg/rec/AppRoutes.scala
+++ job-matcher/src/main/scala/dpg/rec/AppRoutes.scala
@@ -25,40 +25,40 @@ object AppRoutes {
             "Iedere dag krijg je te maken met uitdagende IT-vraagstukken die rechtstreeks beroep doen op je eigen kennisniveau en oplossend vermogen. Het samenwerken met je collega's voelt voor jou als een vis in het water in een omgeving waar ruimte is voor iedereen. Met je enthousiasme ben je altijd op zoek naar de best passende oplossing voor onze klanten.",
             "Centric",
             "Enschede",
-            "https://github.com/MauritsR/hackathon-dpg/blob/main/job-matcher/src/main/resources/attachements/logos/logo_centric.png",
-            "https://github.com/MauritsR/hackathon-dpg/blob/main/job-matcher/src/main/resources/attachements/photos/photo_centric.jpeg"
+            "https://raw.githubusercontent.com/MauritsR/hackathon-dpg/main/job-matcher/src/main/resources/attachements/logos/logo_centric.png?token=GHSAT0AAAAAABRFNBDBIAWXUK7ACZOCOXPAY3LUNBQ",
+            "https://raw.githubusercontent.com/MauritsR/hackathon-dpg/main/job-matcher/src/main/resources/attachements/photos/photo_centric.jpeg?token=GHSAT0AAAAAABRFNBDAOBIHVWMOVUEJA3YQY3LUNRQ"
           ),
           JobSummary(
             "Servicemonteur Verwarmingsinstallatie",
             "Wil jij zorgen voor een beter klimaat? Vind jij duurzaamheid belangrijk en werk jij graag met milieuvriendelijke technologie?",
             "VNOM",
             "Amsterdam",
-            "https://github.com/MauritsR/hackathon-dpg/blob/main/job-matcher/src/main/resources/attachements/logos/logo_vnom.jpeg",
-            "https://github.com/MauritsR/hackathon-dpg/blob/main/job-matcher/src/main/resources/attachements/photos/photo_vnom.jpeg"
+            "https://raw.githubusercontent.com/MauritsR/hackathon-dpg/main/job-matcher/src/main/resources/attachements/logos/logo_vnom.jpeg?token=GHSAT0AAAAAABRFNBDAIIEK6LMK36GEO3UEY3LUOOQ",
+            "https://raw.githubusercontent.com/MauritsR/hackathon-dpg/main/job-matcher/src/main/resources/attachements/photos/photo_vnom.jpeg?token=GHSAT0AAAAAABRFNBDBIP5U2QL7WD3QDXZYY3LUPIA"
           ),
           JobSummary(
             "Facilitair Manager",
             "Het overtreffen van de verwachtingen van onze klanten, daar draait het bij ons om. Supply Chain in de Food Retail is topsport. Dag in, dag uit, hard en vooral slim werken om elk product op het juiste moment op de juiste plaats te krijgen.",
             "Jumbo Supermarkten",
             "Nieuwegein",
-            "https://github.com/MauritsR/hackathon-dpg/blob/main/job-matcher/src/main/resources/attachements/logos/logo_jumbo.png",
-            "https://github.com/MauritsR/hackathon-dpg/blob/main/job-matcher/src/main/resources/attachements/photos/photo_jumbo.jpeg"
+            "https://raw.githubusercontent.com/MauritsR/hackathon-dpg/main/job-matcher/src/main/resources/attachements/logos/logo_jumbo.png?token=GHSAT0AAAAAABRFNBDAZH2PVEVIPQ5QEPVAY3LUPXA",
+            "https://raw.githubusercontent.com/MauritsR/hackathon-dpg/main/job-matcher/src/main/resources/attachements/photos/photo_jumbo.jpeg?token=GHSAT0AAAAAABRFNBDBWXYCAHCYGVHASFOEY3LUQEA"
           ),
           JobSummary(
             "Scala developer",
             "As a Scala engineer you will work on building the next generation marketplace platform for Nationale Vacaturebank & Intermediair. With our two well-known award winning brands in the Dutch labour market: Intermediair & Nationale Vacaturebank, we help thousands of people find the perfect job and help a multitude of companies find the perfect candidate.",
             "DPG Media",
             "Amsterdam",
-            "https://github.com/MauritsR/hackathon-dpg/blob/main/job-matcher/src/main/resources/attachements/logos/logo_dpg_recruitment.png",
-            "https://github.com/MauritsR/hackathon-dpg/blob/main/job-matcher/src/main/resources/attachements/photos/photo_dpgmedia_1.png"
+            "https://raw.githubusercontent.com/MauritsR/hackathon-dpg/main/job-matcher/src/main/resources/attachements/logos/logo_dpg_recruitment.png?token=GHSAT0AAAAAABRFNBDALXZKF4BJDC572C6EY3LUQRQ",
+            "https://raw.githubusercontent.com/MauritsR/hackathon-dpg/main/job-matcher/src/main/resources/attachements/photos/photo_dpgmedia_1.png?token=GHSAT0AAAAAABRFNBDBP6RXKGVND5FWEMR4Y3LURBQ"
           ),
           JobSummary(
             "React developer",
             "Do you want to develop a fast and seamless experience for two million users a month? Our goal is helping our users to find their next job.",
             "DPG Media",
             "Amsterdam",
-            "https://github.com/MauritsR/hackathon-dpg/blob/main/job-matcher/src/main/resources/attachements/logos/logo_dpg_recruitment.png",
-            "https://github.com/MauritsR/hackathon-dpg/blob/main/job-matcher/src/main/resources/attachements/photos/photo_dpgmedia_2.jpeg"
+            "https://raw.githubusercontent.com/MauritsR/hackathon-dpg/main/job-matcher/src/main/resources/attachements/logos/logo_dpg_recruitment.png?token=GHSAT0AAAAAABRFNBDALXZKF4BJDC572C6EY3LUQRQ",
+            "https://raw.githubusercontent.com/MauritsR/hackathon-dpg/main/job-matcher/src/main/resources/attachements/photos/photo_dpgmedia_2.jpeg?token=GHSAT0AAAAAABRFNBDADUUPHWD4G5WK6DKKY3LURPQ"
           )
         )
       Ok(jobs.asJson)
