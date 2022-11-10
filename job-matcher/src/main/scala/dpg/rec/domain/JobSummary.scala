50fd07106babbb48cbae4fe251e8e406882002d1
Ghazi Naceur <ghazi.naceur@persgroep.net>
2022-11-10 11:08:10 +0100

    Add job_characteristics properties for JobSummary

--- job-matcher/src/main/scala/dpg/rec/domain/JobSummary.scala
+++ job-matcher/src/main/scala/dpg/rec/domain/JobSummary.scala
@@ -1,3 +1,14 @@
 package dpg.rec.domain
 
-case class JobSummary(id: Int, jobTitle: String, description: String, company: String, location: String, logo: String, photo: String)
+case class JobSummary(
+  id: Int,
+  jobTitle: String,
+  description: String,
+  jobCharacteristic1: String,
+  jobCharacteristic2: String,
+  jobCharacteristic3: String,
+  company: String,
+  location: String,
+  logo: String,
+  photo: String
+)
